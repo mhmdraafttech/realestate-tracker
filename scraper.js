7af0d1043027e9ef28e4bb9d95dd45283574df3d
mhmdraafttech <mhmdraaft.tech@gmail.com>
2026-06-10 11:51:59 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,5 +1,7 @@
 import { chromium } from "playwright";
 
+console.log("START");
+
 async function scrape() {
   const browser = await chromium.launch({
     headless: true
@@ -10,13 +12,22 @@ async function scrape() {
   await page.goto(
     "https://www.realestate.com.au/buy/in-sydney,+nsw+2000/list-1",
     {
-      waitUntil: "networkidle"
+      waitUntil: "domcontentloaded"
     }
   );
 
+  console.log("TITLE:");
   console.log(await page.title());
+
+  console.log("URL:");
+  console.log(page.url());
 
   await browser.close();
 }
 
-scrape();
+scrape()
+  .then(() => console.log("DONE"))
+  .catch(err => {
+    console.error("ERROR:");
+    console.error(err);
+  });
